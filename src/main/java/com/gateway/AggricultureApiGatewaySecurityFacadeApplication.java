aaa2a5a069a622ecd5fa5636a973c5a22b1ae447
Anuj Singh <anujs503@gmail.com>
2021-08-18 22:34:23 +0100

    Project Initialized

---
+++ src/main/java/com/gateway/AggricultureApiGatewaySecurityFacadeApplication.java
@@ -0,0 +1,13 @@
+package com.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggricultureApiGatewaySecurityFacadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AggricultureApiGatewaySecurityFacadeApplication.class, args);
+	}
+
+}
